143c8d7a29317bd3bce5378ab74d3d0d03cc8b02
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-06-21 18:58:59 -0400

    Formatted server.js

--- js/server.js
+++ js/server.js
@@ -26,17 +26,17 @@ const User = connection.define('user', {
 const Review = connection.define('review', {
     text: { type: Sequelize.TEXT, unique: true, allowNull: false },
     timestamp: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
-    user_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
         }
     },
-    venue_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    venue_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: Venue,
             key: 'id'
@@ -47,19 +47,19 @@ const Review = connection.define('review', {
 const Venue = connection.define('venue', {
     name: { type: Sequelize.STRING, unique: true, allowNull: false },
     address: { type: Sequelize.STRING, unique: true, allowNull: false },
-    phonenumber: { type:Sequelize.INTEGER, unique: true, allowNull: true },
+    phonenumber: { type: Sequelize.INTEGER, unique: true, allowNull: true },
     website: { type: Sequelize.STRING, unique: true, allowNull: true },
-    event_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: true, 
+    event_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
         references: {
             model: Event,
             key: 'id'
         }
     },
-    user_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
@@ -70,25 +70,25 @@ const Venue = connection.define('venue', {
 const Comment = connection.define('comment', {
     text: { type: Sequelize.TEXT, unique: true, allowNull: false },
     timestamp: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
-    user_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
         }
     },
-    venue_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    venue_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: Venue,
             key: 'id'
         }
     },
-    review_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    review_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: Review,
             key: 'id'
@@ -100,11 +100,11 @@ const Resource = connection.define('resource', {
     name: { type: Sequelize.STRING, unique: true, allowNull: false },
     description: Sequelize.TEXT,
     address: { type: Sequelize.STRING, unique: true, allowNull: true },
-    phonenumber: { type:Sequelize.INTEGER, unique: true, allowNull: true },
+    phonenumber: { type: Sequelize.INTEGER, unique: true, allowNull: true },
     website: { type: Sequelize.STRING, unique: true, allowNull: true },
-    user_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
@@ -117,20 +117,20 @@ const Event = connection.define('event', {
     timestamp: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
     description: Sequelize.TEXT,
     address: { type: Sequelize.STRING, allowNull: true },
-    phonenumber: { type:Sequelize.INTEGER, allowNull: true },
+    phonenumber: { type: Sequelize.INTEGER, allowNull: true },
     website: { type: Sequelize.STRING, allowNull: true },
     eventdate: Sequelize.DATEONLY,
-    user_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: false, 
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
         }
     },
-    venue_id: { 
-        type: Sequelize.INTEGER, 
-        allowNull: true, 
+    venue_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
         references: {
             model: Venue,
             key: 'id'
@@ -141,12 +141,12 @@ const Event = connection.define('event', {
 connection.sync();
 
 connection
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
   // sequelize.TEXT = much longer string than the string data type
