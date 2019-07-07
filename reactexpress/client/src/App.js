3cffcdee10390089886b21a62fb1f98236d6363f
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-07-07 13:13:40 -0400

    Followed tutorial from Traversy Media to create boilerplate template connecting React and Express using dummy data and set up npm run dev command using concurrently to start front and back end with a single command

---
+++ reactexpress/client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/users/users.css';
+import Users from './components/users/users';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Users />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactexpress/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/users', (req, res) => {
+    const users = [
+        { id: 1, userName: 'kelly' },
+        { id: 2, userName: 'drew' },
+        { id: 3, userName: 'caitlin' }
+    ];
+    res.json(users);
+});
+// req = request and res = response
+// hardcoding an array of objects to be fetched by the get request to check that our front end and back end are connected  -> will change this to database connection later on
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
